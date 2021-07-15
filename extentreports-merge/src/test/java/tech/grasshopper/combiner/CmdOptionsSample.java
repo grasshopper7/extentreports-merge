434ffaf6cbe5e73a2ae55c526c3bf73344ecf246
grasshopper7 <grass.hopper.moc@gmail.com>
2021-07-15 15:38:16 +0530

    Refactored cmd sample

--- extentreports-merge/src/test/java/tech/grasshopper/combiner/CmdOptionsSample.java
+++ extentreports-merge/src/test/java/tech/grasshopper/combiner/CmdOptionsSample.java
@@ -5,15 +5,37 @@ import org.junit.Test;
 public class CmdOptionsSample {
 
 	@Test
-	public void cmdOptionsCombine() {
+	public void cmdDefaultOptionsCombine() {
 		String[] args = new String[6];
 
 		args[0] = "-jsonReportPaths";
 		args[1] = "sample/combine/cmdline/ExtentJsonOne.json, sample/combine/cmdline/ExtentJsonTwo.json, sample/combine/cmdline/ExtentJsonThree.json";
 		args[2] = "-mergedReportDirPath";
-		args[3] = "sample/combine/cmdline/";
+		args[3] = "sample/combine/cmdline/default";
 		args[4] = "-configType";
 		args[5] = "xml";
+
+		Combiner.main(args);
+	}
+
+	@Test
+	public void cmdSpecifiedOptionsCombine() {
+		String[] args = new String[14];
+
+		args[0] = "-reportType";
+		args[1] = "combine";
+		args[2] = "-extraStrategy";
+		args[3] = "add";
+		args[4] = "-matchingStrategy";
+		args[5] = "later";
+		args[6] = "-primaryJsonStrategy";
+		args[7] = "default";
+		args[8] = "-secondaryJsonStrategy";
+		args[9] = "default";
+		args[10] = "-jsonReportPaths";
+		args[11] = "sample/combine/cmdline/ExtentJsonOne.json, sample/combine/cmdline/ExtentJsonTwo.json, sample/combine/cmdline/ExtentJsonThree.json";
+		args[12] = "-mergedReportDirPath";
+		args[13] = "sample/combine/cmdline/specified";
 
 		Combiner.main(args);
 	}
